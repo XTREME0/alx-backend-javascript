52ed5cd62d016adc31999138453e06259de6d73f
xtreme <anastaai94@gmail.com>
2024-05-29 20:26:20 +0100

    fixed conf files

--- 0x01-ES6_promise/utils.js
+++
@@ -1,9 +0,0 @@
-function uploadPhoto() {
-  return Promise.resolve('Photo uploaded');
-}
-
-function createUser() {
-  return Promise.resolve('User created');
-}
-
-module.exports = { uploadPhoto, createUser };
