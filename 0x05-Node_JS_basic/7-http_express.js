06fe5ca89cba77e78401ea14169347b63dba2a57
xtreme <anastaai94@gmail.com>
2024-07-23 20:09:14 +0100

    task07

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,29 +1,63 @@
 const express = require('express');
+
+const { readFile } = require('fs');
+
 const app = express();
-const students = require('./3-read_file_async');
 const port = 1245;
-const hostname = '127.0.0.1';
+
+function cStudents(file) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((res, rej) => {
+    readFile(file, (err, data) => {
+      if (err) {
+        rej(err);
+      } else {
+        let o = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        o += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            o += `Number of students in ${key}: ${value}. `;
+            o += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        res(o);
+      }
+    });
+  });
+}
 
 app.get('/', (req, resp) => {
-  resp.statusCode = 200;
-  resp.setHeader('Content-Type', 'text/plain');
   resp.send('Hello Holberton School!');
 });
-
-app.get('/students', async (req, resp) => {
-  resp.statusCode = 200;
-  resp.setHeader('Content-Type', 'text/plain');
-  resp.write('This is the list of our students\n');
-  await students(process.argv[2]).then((data) => {
-    resp.write(`Number of students: ${data.students.length}\n`);
-    resp.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    resp.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-  }).catch((err) => resp.write(err.message))
-    .finally(() => {
-      resp.end();
-    });
+app.get('/students', (req, resp) => {
+  cStudents(process.argv[2].toString()).then((o) => {
+    resp.send(['This is the list of our students', o].join('\n'));
+  }).catch(() => {
+    resp.send('This is the list of our students\nCannot load the database');
+  });
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+app.listen(port, () => {
 });
+
+module.exports = app;
