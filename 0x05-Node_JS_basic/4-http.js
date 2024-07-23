a0853377babb38cb03ee789d9208bc4049e2147c
xtreme <anastaai94@gmail.com>
2024-07-23 19:42:12 +0100

    task04

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const wapp = http.createServer((req, resp) => {
+  resp.writeHead(200, { 'Content-Type': 'text/plain' });
+  resp.end('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('Server is running on http://localhost:1245');
+});
+
+module.exports = wapp;
