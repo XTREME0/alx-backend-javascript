190bbd7726268c7468bdc8966d30f81e03ede2b4
xtreme <anastaai94@gmail.com>
2024-07-25 21:11:16 +0100

    task5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,23 @@
+const {describe, it} = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./5-payment");
+const expect = require("chai").expect;
+
+describe("sendPaymentRequestToApi", function() {
+    beforeEach("Set up spy", function() {
+	sinon.spy(console, "log");
+    });
+    afterEach("restore spy", function() {
+	console.log.restore();
+    });
+    it("check if the right arg were passed", function() {
+	sendPaymentRequestToApi(100, 20);
+
+	expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
+    });
+    it("check if the right arg were passed", function() {
+	sendPaymentRequestToApi(10, 10);
+
+	expect(console.log.withArgs("The total is: 20").calledOnce).to.be.true;
+    });
+});
