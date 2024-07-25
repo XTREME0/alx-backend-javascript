b3e142b81c581312614aad6967afd4cefa280122
xtreme <anastaai94@gmail.com>
2024-07-25 20:27:51 +0100

    task2

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,15 +3,15 @@ const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function () {
     it('return the sum of numbers rounded off', function () {
-        assert.strictEqual(calculateNumber('SUM', 2.4, 3.6), 6);
+        assert.strictEqual(calculateNumber('SUM', 2.4, 3.4), 5);
     });
 
     it('return the difference of numbers rounded off', function () {
-        assert.strictEqual(calculateNumber('SUBTRACT', 2.4, 3.6), -2);
+        assert.strictEqual(calculateNumber('SUBTRACT', 2.4, 3.1), -1);
     });
 
     it('division of numbers rounded off', function () {
-        assert.strictEqual(calculateNumber('DIVIDE', 2.4, 1.6), 1);
+        assert.strictEqual(calculateNumber('DIVIDE', 2.4, 1.3), 2);
     });
 
     it('rounded b is 0', function () {
